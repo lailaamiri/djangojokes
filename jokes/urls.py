d1bc2a21920b1626b7fd33db5b7d7a51a4a9e508
LailaAmiri <>
2022-12-09 11:28:15 +0100

    opdracht 14

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import JokeListView,JokeDetailView
+from .views import JokeListView,JokeDetailView,JokeCreateView,JokeUpdateView
 
 app_name = 'jokes'
 urlpatterns = [ 
     path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
+    path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name='update'),
+    path('joke/create/', JokeCreateView.as_view(), name='create'),
     path('', JokeListView.as_view(), name='list'),
     ]
--- jokes/views.py
+++ jokes/views.py
@@ -1,7 +1,16 @@
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from .models import Joke
 
 class JokeDetailView(DetailView):
     model = Joke
+
 class JokeListView(ListView):
     model = Joke
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
