83e2d6a2b49a37a9833b1006164007d957543d46
LailaAmiri <>
2022-12-09 11:06:55 +0100

    opdracht13

--- jokes/views.py
+++ jokes/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Joke
 
+class JokeDetailView(DetailView):
+    model = Joke
 class JokeListView(ListView):
     model = Joke
